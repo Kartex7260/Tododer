b05eac0f95375d2a68653c7a4de5a62d9341c45f
Kirill Zhitko <kirillzhitko@gmail.com>
2023-10-04 22:07:32 +0300

    Создание и настройка проекта

---
+++ app/build.gradle.kts
@@ -0,0 +1,89 @@
+import org.jetbrains.kotlin.gradle.internal.KaptGenerateStubsTask
+
+plugins {
+	id("com.android.application")
+	id("org.jetbrains.kotlin.android")
+
+	id("org.jetbrains.kotlin.kapt")
+	id("com.google.devtools.ksp")
+
+	id("com.google.dagger.hilt.android")
+}
+
+tasks.withType<KaptGenerateStubsTask>().configureEach {
+	kotlinOptions {
+		jvmTarget = "1.8"
+	}
+}
+
+android {
+	namespace = "kanti.tododer"
+	compileSdk = 33
+
+	defaultConfig {
+		applicationId = "kanti.tododer"
+		minSdk = 24
+		targetSdk = 33
+		versionCode = 1
+		versionName = "1.0"
+
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+	}
+
+	buildTypes {
+		release {
+			isMinifyEnabled = false
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt"),
+				"proguard-rules.pro"
+			)
+		}
+	}
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_1_8
+		targetCompatibility = JavaVersion.VERSION_1_8
+	}
+	kotlinOptions {
+		jvmTarget = "1.8"
+	}
+	buildFeatures {
+		viewBinding = true
+	}
+}
+
+dependencies {
+
+	val hiltVersion = "2.48"
+	val roomVersion = "2.5.2"
+	val lifecycleVersion = "2.6.2"
+	val navigationVersion = "2.7.4"
+
+	// common
+	testImplementation("junit:junit:4.13.2")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+	// ui
+	implementation("androidx.core:core-ktx:1.12.0")
+	implementation("androidx.appcompat:appcompat:1.6.1")
+	implementation("com.google.android.material:material:1.9.0")
+	androidTestImplementation("androidx.test.ext:junit:1.1.5")
+	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+	implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+	implementation("androidx.cardview:cardview:1.0.0")
+	implementation("androidx.recyclerview:recyclerview:1.3.1")
+	implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+	implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+	implementation("androidx.fragment:fragment-ktx:1.6.1")
+	implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
+	implementation("androidx.navigation:navigation-ui-ktx:$navigationVersion")
+	implementation("androidx.preference:preference-ktx:1.2.1")
+
+	// di
+	implementation("com.google.dagger:hilt-android:$hiltVersion")
+	kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
+
+	// data
+	implementation("androidx.room:room-runtime:$roomVersion")
+	implementation("androidx.room:room-ktx:$roomVersion")
+	ksp("androidx.room:room-compiler:$roomVersion")
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Tododer
+
+## 1. Используемые технологии
+
+1. MVVM (ViewModel, LiveData, Lifecycle)
+2. Coroutines, Flow
+3. Dagger/Hilt
+4. Room
+5. Preference API
+6. RecyclerView
+7. Fragment, Navigation
