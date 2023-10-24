4b324c57a6acbbb6d7873e584db905f94e88129d
Kirill Zhitko <kirillzhitko@gmail.com>
2023-10-24 17:49:32 +0300

    FEAT: возврат к предыдущему Todo элементу при Back pressed

--- app/src/main/java/kanti/tododer/ui/screens/todo_detail/TodoDetailScreenFragment.kt
+++ app/src/main/java/kanti/tododer/ui/screens/todo_detail/TodoDetailScreenFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -54,6 +55,10 @@ class TodoDetailScreenFragment : Fragment() {
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
+
+		requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+			viewModel.pop()
+		}
 
 		viewLifecycleOwner.lifecycleScope.launch {
 			repeatOnLifecycle(Lifecycle.State.STARTED) {
