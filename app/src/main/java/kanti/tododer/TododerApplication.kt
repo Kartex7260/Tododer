09387766e6c407259946c1c92956598ea96b8811
Kirill Zhitko <kirillzhitko@gmail.com>
2023-11-01 15:25:19 +0300

    FEAT: подготовка приложения к использованию
    Изменение readme.md

--- app/src/main/java/kanti/tododer/TododerApplication.kt
+++ app/src/main/java/kanti/tododer/TododerApplication.kt
@@ -19,91 +19,9 @@ import javax.inject.Inject
 @HiltAndroidApp
 class TododerApplication : Application() {
 
-	@Inject lateinit var planRepository: IPlanRepository
-	@Inject lateinit var taskRepository: ITaskRepository
-
 	override fun onCreate() {
 		super.onCreate()
 		Const.init(this)
-
-		CoroutineScope(Dispatchers.Default).launch {
-			val job = deleteAll()
-			job.join()
-			Plan(
-				title = "Продукты",
-				remark = "Купить"
-			).addR().value?.apply {
-				Plan(
-					title = "Пятёрочка",
-					remark = "А где она?"
-				).add(this).value?.apply {
-					Task(
-						title = "Хлеб"
-					).add(this)
-					Task(
-						title = "Молоко"
-					).add(this)
-				}
-				Task(
-					title = "Детский мир"
-				).add(this)
-				Task(
-					title = "Авокадо",
-					done = true
-				).add(this)
-				Plan(
-					title = "Лента"
-				).add(this).value?.apply {
-					Task(
-						title = "Бананы"
-					)
-				}
-			}
-			Plan(
-				title = "Работа"
-			).addR().value?.apply {
-				Task(
-					title = "Сделать то то"
-				).add(this)
-				Task(
-					title = "Сделать сё то"
-				).add(this)
-			}
-		}
-
-	}
-
-	private fun CoroutineScope.deleteAll(): Job {
-		return launch {
-			planRepository.deleteAll()
-			taskRepository.deleteAll()
-		}
-	}
-
-	private suspend fun Plan.addR(): RepositoryResult<Plan> = planRepository.insertToRoot(this)
-
-	private suspend fun Plan.add(plan: Plan): RepositoryResult<Plan> {
-		return planRepository.replace(this) {
-			copy(
-				parentId = plan.fullId
-			)
-		}
-	}
-
-	private suspend fun Task.add(plan: Plan): RepositoryResult<Task> {
-		return taskRepository.replace(this) {
-			copy(
-				parentId = plan.fullId
-			)
-		}
-	}
-
-	private suspend fun Task.add(task: Task): RepositoryResult<Task> {
-		return taskRepository.replace(this) {
-			copy(
-				parentId = task.fullId
-			)
-		}
 	}
 
 }
--- readme.md
+++ readme.md
@@ -2,10 +2,25 @@
 
 ## 1. Используемые технологии
 
-1. MVVM (ViewModel, LiveData, Lifecycle)
+1. MVVM (ViewModel, LiveData/StateFlow/SharedFlow, Lifecycle)
 2. Coroutines, Flow
 3. Dagger/Hilt
 4. Room
 5. Preference API
 6. RecyclerView
 7. Fragment, Navigation
+
+## 2. Технические особенности
+
+* Работа с локальной базой данных
+* Бесконечное дерево вложености планов и задач
+* Повторное использование Fragment
+* CustomView
+
+## 3. Реализованные фичи
+
+* [X]  Создание, изменение, удаление планов и задач
+* [X]  Алгоритмическое вычисление состояние выполнения плана
+* [ ]  Настройки / изменение темы
+* [ ]  Архив планов и задач
+* [ ]  Облачное хранение
