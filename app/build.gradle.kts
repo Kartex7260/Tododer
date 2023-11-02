9ff87634a83261e64920f7d01b157e00737c9126
Kirill Zhitko <kirillzhitko@gmail.com>
2023-11-02 22:29:12 +0300

    FEAT: иконка приложения

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,9 +25,9 @@ android {
 	defaultConfig {
 		applicationId = "kanti.tododer"
 		minSdk = 24
-		targetSdk = 33
+		targetSdk = 34
 		versionCode = 1
-		versionName = "1.0"
+		versionName = "1.0.0"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
