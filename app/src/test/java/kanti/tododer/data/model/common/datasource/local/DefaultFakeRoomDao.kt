6a39d877fa92712dd24f7e90e3cd44cf90b56dfa
Kirill Zhitko <kirillzhitko@gmail.com>
2023-11-25 09:52:10 +0300

    REFACTOR: очистка и создание тестов с нуля

---
+++ app/src/test/java/kanti/tododer/data/model/common/datasource/local/DefaultFakeRoomDao.kt
@@ -0,0 +1,116 @@
+package kanti.tododer.data.model.common.datasource.local
+
+import kanti.tododer.data.model.common.Todo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+
+class DefaultFakeRoomDao<T>(
+	private val newId: T.(id: Int) -> T,
+	val todos: MutableList<T> = mutableListOf()
+) where T : Todo {
+
+	suspend fun getChildrenRoom(parentId: String): List<T> {
+		return async {
+			todos.filter { it.parentId == parentId }
+		}
+	}
+
+	suspend fun getByRowIdRoom(rowId: Long): T? {
+		return async {
+			todos.getOrNull(rowId.toInt())
+		}
+	}
+
+	suspend fun getTodoRoom(id: Int): T? {
+		return async {
+			todos.firstOrNull { it.id == id }
+		}
+	}
+
+	suspend fun insertRoom(vararg todo: T): Array<Long> {
+		val rowIds = mutableListOf<Long>()
+		fun addTodo(id: Int, todo: T): Int {
+			if (containsTask(todo))
+				return id
+			return if (todo.id == 0) {
+				val newId = id + 1
+				todos.add(todo.newId(newId))
+				rowIds.add(todos.indexOfFirst { it.id == newId }.toLong())
+				newId
+			} else {
+				todos.add(todo)
+				rowIds.add(todos.indexOfFirst { it.id == todo.id }.toLong())
+				todo.id
+			}
+		}
+
+		launch {
+			var lastId = todos.lastOrNull()?.id ?: 0
+			for (td in todo) {
+				lastId = addTodo(lastId, td)
+			}
+		}
+		return rowIds.toTypedArray()
+	}
+
+	suspend fun updateRoom(vararg todo: T): Int {
+		return async {
+			var result = 0
+			for (tsk in todo) {
+				val index = todos.indexOfFirst { it.id == tsk.id }
+				if (index == -1) {
+					continue
+				}
+				todos[index] = tsk
+				result++
+			}
+			result
+		}
+	}
+
+	suspend fun deleteRoom(vararg todo: T): Int {
+		return async {
+			var result = 0
+			for (tsk in todo) {
+				val removeResult = todos.removeIf { it.id == tsk.id }
+				if (removeResult)
+					result++
+			}
+			result
+		}
+	}
+
+	suspend fun deleteAllRoom() {
+		launch {
+			todos.clear()
+		}
+	}
+
+	private fun containsTask(task: T): Boolean {
+		if (task.id == 0)
+			return false
+		return todos.firstOrNull { it.id == task.id } != null
+	}
+
+	private suspend fun <T> async(block: suspend CoroutineScope.() -> T): T {
+		return coroutineScope {
+			async(
+				context = Dispatchers.Default,
+				block = block
+			).await()
+		}
+	}
+
+	private suspend fun launch(block: suspend CoroutineScope.() -> Unit) {
+		return coroutineScope {
+			launch(
+				context = Dispatchers.Default,
+				block = block
+			)
+		}
+	}
+
+}
--- app/src/test/java/kanti/tododer/data/model/task/datasource/local/FakeTaskDao.kt
+++ app/src/test/java/kanti/tododer/data/model/task/datasource/local/FakeTaskDao.kt
@@ -1,81 +1,45 @@
 package kanti.tododer.data.model.task.datasource.local
 
-class FakeTaskDao : TaskDao() {
+import kanti.tododer.data.model.common.datasource.local.DefaultFakeRoomDao
 
-	private val tasks = mutableListOf<TaskEntity>()
+class FakeTaskDao(
+	val tasks: MutableList<TaskEntity> = mutableListOf()
+) : TaskDao {
 
-	override suspend fun getChildren(parentId: String): List<TaskEntity> {
-		return tasks.filter { it.parentId == parentId }
+	private val baseDao = DefaultFakeRoomDao(
+		newId = { newId ->
+			toTaskEntity(
+				id = newId
+			)
+		},
+		todos = tasks
+	)
+
+	override suspend fun getChildrenRoom(parentId: String): List<TaskEntity> {
+		return baseDao.getChildrenRoom(parentId)
 	}
 
-	override suspend fun getByRowId(rowId: Long): TaskEntity? {
-		if (rowId < 0)
-			return null
-		return tasks[rowId.toInt()]
+	override suspend fun getByRowIdRoom(rowId: Long): TaskEntity? {
+		return baseDao.getByRowIdRoom(rowId)
 	}
 
-	override suspend fun getTask(id: Int): TaskEntity? {
-		return tasks.firstOrNull { it.id == id }
+	override suspend fun getTaskRoom(id: Int): TaskEntity? {
+		return baseDao.getTodoRoom(id)
 	}
 
-	override suspend fun replace(task: TaskEntity): Long {
-		val taskWithId = taskWithId(task)
-		fun rowId() = tasks.size.toLong() - 1
-		if (task.id == 0) {
-			tasks.add(taskWithId)
-			return rowId()
-		} else {
-			val rowId = tasks.indexOfFirst { it.id == taskWithId.id }
-			if (rowId == -1) {
-				tasks.add(taskWithId)
-				return rowId()
-			}
-			tasks[rowId] = taskWithId
-			return tasks.indexOfFirst { it.id == task.id }.toLong()
-		}
+	override suspend fun insertRoom(vararg task: TaskEntity): Array<Long> {
+		return baseDao.insertRoom(*task)
 	}
 
-	override suspend fun insert(task: TaskEntity): Long {
-		if (tasks.firstOrNull { it.id == task.id } != null)
-			return -1
-		val taskWithId = taskWithId(task)
-		fun rowId() = tasks.size.toLong() - 1
-		if (task.id == 0) {
-			tasks.add(taskWithId)
-			return rowId()
-		} else {
-			val rowId = tasks.indexOfFirst { it.id == taskWithId.id }
-			if (rowId == -1) {
-				tasks.add(taskWithId)
-				return rowId()
-			}
-			return -1
-		}
+	override suspend fun updateRoom(vararg task: TaskEntity): Int {
+		return baseDao.updateRoom(*task)
 	}
 
-	override suspend fun delete(task: TaskEntity): Int {
-		return if (tasks.removeIf { it.id == task.id }) 1 else 0
+	override suspend fun deleteRoom(vararg task: TaskEntity): Int {
+		return baseDao.deleteRoom(*task)
 	}
 
-	override suspend fun deleteAll() {
-		tasks.clear()
+	override suspend fun deleteAllRoom() {
+		baseDao.deleteAllRoom()
 	}
-
-	private fun taskWithId(task: TaskEntity): TaskEntity {
-		return if (task.id != 0) {
-			task
-		} else {
-			task.copy(id = tasks.size + 1)
-		}
-	}
-
-	fun clear() {
-		tasks.clear()
-	}
-
-	val isEmpty: Boolean
-		get() {
-			return tasks.isEmpty()
-		}
-
 }
