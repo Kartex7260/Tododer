5604dc6ab78600a67f1523e501cb6636a8952df9
Kirill Zhitko <kirillzhitko@gmail.com>
2023-12-18 20:32:46 +0300

    TEST: PlanRepositoryImpl

---
+++ data/plan/api/src/test/java/kanti/tododer/data/model/plan/PlanRepositoryImplTest.kt
@@ -0,0 +1,128 @@
+package kanti.tododer.data.model.plan
+
+import kanti.tododer.data.model.plan.datasource.local.DefaultPlanInitializer
+import kanti.tododer.data.model.plan.datasource.local.FakePlanLocalDataSource
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class PlanRepositoryImplTest {
+
+	private val plans: MutableMap<Int, Plan> = LinkedHashMap()
+	private val repository: PlanRepository = PlanRepositoryImpl(FakePlanLocalDataSource(
+		planInitializer = DefaultPlanInitializer(),
+		plans = plans
+	))
+
+	@AfterEach
+	fun afterEach() = runTest {
+		plans.clear()
+	}
+
+	@Test
+	@DisplayName("create(String)")
+	fun create() = runTest {
+		val expectedPlan = Plan(id = 1, title = "Test")
+	    val plan = repository.create("Test")
+
+		val expectedArray = arrayOf(
+			Plan(id = 1, title = "Test")
+		)
+
+		assertEquals(expectedPlan, plan)
+		assertArrayEquals(expectedArray, plans.values.toTypedArray())
+	}
+
+	@Test
+	@DisplayName("updateTitle(Plan, String)")
+	fun updateTitle() = runTest {
+		val expectedPlan = Plan(id = 1, title = "Updated")
+		val expectedArray = arrayOf(Plan(id = 1, title = "Updated"))
+	    plans.putAll(mapOf(
+			1 to Plan(id = 1, title = "Test 1")
+		))
+
+		val plan = repository.updateTitle(Plan(id = 1), title = "Updated")
+
+		assertEquals(expectedPlan, plan)
+		assertArrayEquals(expectedArray, plans.values.toTypedArray())
+	}
+
+	@Test
+	@DisplayName("archive(Plan)")
+	fun archive() = runTest {
+		val expectedArray = arrayOf(Plan(id = 1, archived = true))
+		plans[1] = Plan(id = 1, archived = false)
+
+		repository.archive(listOf(
+			Plan(id = 1, archived = false),
+			Plan(id = 2, archived = false)
+		))
+		assertArrayEquals(expectedArray, plans.values.toTypedArray())
+	}
+
+	@Test
+	@DisplayName("unarchive(Plan)")
+	fun unarchive() = runTest {
+		val expectedArray = arrayOf(Plan(id = 1, archived = false))
+		plans[1] = Plan(id = 1, archived = true)
+
+		repository.unarchive(listOf(
+			Plan(id = 1, archived = true),
+			Plan(id = 2, archived = true)
+		))
+		assertArrayEquals(expectedArray, plans.values.toTypedArray())
+	}
+
+	@Test
+	@DisplayName("delete(List<Plan>)")
+	fun delete() = runTest {
+	    plans.putAll(mapOf(
+			1 to Plan(id = 1),
+			2 to Plan(id = 2),
+			3 to Plan(id = 3)
+		))
+		val expectedArray = arrayOf(Plan(id = 2))
+
+		repository.delete(listOf(Plan(id = 1), Plan(id = 3), Plan(id = 4)))
+		assertArrayEquals(expectedArray, plans.values.toTypedArray())
+	}
+
+	@Test
+	@DisplayName("init()")
+	fun init() = runTest {
+	    repository.init()
+		assertNotEquals(0, plans.size)
+	}
+
+	@Test
+	@DisplayName("isEmpty() true")
+	fun isEmptyTrue() = runTest {
+	    val actual = repository.isEmpty()
+		assertTrue(actual)
+	}
+
+	@Test
+	@DisplayName("isEmpty() false")
+	fun isEmptyFalse() = runTest {
+	    plans[1] = Plan(id = 1)
+		val actual = repository.isEmpty()
+		assertFalse(actual)
+	}
+
+	@Test
+	@DisplayName("clear()")
+	fun clear() = runTest {
+	    plans.putAll(mapOf(
+			1 to Plan(id = 1),
+			2 to Plan(id = 2),
+			3 to Plan(id = 3)
+		))
+
+		repository.clear()
+		val actual = repository.isEmpty()
+		assertFalse(actual)
+	}
+}
---
+++ data/plan/api/src/test/java/kanti/tododer/data/model/plan/datasource/local/FakePlanLocalDataSource.kt
@@ -0,0 +1,68 @@
+package kanti.tododer.data.model.plan.datasource.local
+
+import kanti.tododer.data.model.plan.Plan
+import kanti.tododer.data.model.plan.toPlan
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.map
+
+class FakePlanLocalDataSource(
+	private val planInitializer: PlanInitializer,
+	private val plans: MutableMap<Int, Plan> = LinkedHashMap()
+) : PlanLocalDataSource {
+
+	private val stateFlow: MutableStateFlow<List<Plan>> = MutableStateFlow(plans.values.toList())
+
+	override val standardPlans: Flow<List<Plan>>
+		get() = stateFlow.map { plans -> plans.filter { !it.archived } }
+	override val archivedPlans: Flow<List<Plan>>
+		get() = stateFlow.map { plans -> plans.filter { it.archived } }
+
+	override suspend fun insert(plan: Plan): Plan {
+		val newPlan = if (plan.id == 0) {
+			val lastId = plans.keys.lastOrNull() ?: 0
+			plan.toPlan(id = lastId + 1)
+		} else {
+			if (plans.containsKey(plan.id))
+				throw IllegalArgumentException("Plan(id = ${plan.id}) already exist!")
+			plan
+		}
+		plans[newPlan.id] = newPlan
+		return newPlan
+	}
+
+	override suspend fun update(plan: Plan): Plan {
+		if (!plans.containsKey(plan.id))
+			throw IllegalArgumentException("Not found plan by id=${plan.id}")
+		plans[plan.id] = plan
+		return plan
+	}
+
+	override suspend fun update(plans: List<Plan>) {
+		for (plan in plans) {
+			if (!this.plans.containsKey(plan.id))
+				continue
+			this.plans[plan.id] = plan
+		}
+	}
+
+	override suspend fun delete(plans: List<Plan>) {
+		for (plan in plans) {
+			this.plans.remove(plan.id)
+		}
+	}
+
+	override suspend fun init() {
+		if (isEmpty())
+			planInitializer.initialize(this)
+	}
+
+	override suspend fun isEmpty(): Boolean {
+		return plans.isEmpty()
+	}
+
+	override suspend fun clear() {
+		plans.clear()
+		init()
+	}
+}
