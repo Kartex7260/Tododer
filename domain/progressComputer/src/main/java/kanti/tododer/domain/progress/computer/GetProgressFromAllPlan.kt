3f5ae409d34a029d42de1381d4757886a422a4eb
Kirill Zhitko <kirillzhitko@gmail.com>
2024-02-15 16:18:20 +0300

    FEAT: логгер в ComputePlanProgress и GetProgressFromAllPlan
    FIX: просчёт прогресса плана

--- domain/progressComputer/src/main/java/kanti/tododer/domain/progress/computer/GetProgressFromAllPlan.kt
+++ domain/progressComputer/src/main/java/kanti/tododer/domain/progress/computer/GetProgressFromAllPlan.kt
@@ -1,9 +1,12 @@
 package kanti.tododer.domain.progress.computer
 
+import kanti.tododer.common.logTag
 import kanti.tododer.data.model.plan.Plan
 import kanti.tododer.data.model.plan.PlanRepository
 import kanti.tododer.data.model.progress.PlanProgress
 import kanti.tododer.data.model.progress.ProgressRepository
+import kanti.tododer.util.log.Logger
+import kanti.tododer.util.log.StandardLog
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -15,40 +18,56 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class GetProgressFromAllPlan @Inject constructor(
-	planRepository: PlanRepository,
-	private val progressRepository: ProgressRepository,
-	private val computePlanProgress: ComputePlanProgress
+    planRepository: PlanRepository,
+    private val progressRepository: ProgressRepository,
+    private val computePlanProgress: ComputePlanProgress,
+    @StandardLog private val logger: Logger
 ) {
 
-	private val _update = MutableStateFlow(Any())
+    private val _update = MutableStateFlow(Any())
 
-	val planAllProgress: Flow<Float> = planRepository.planAll.computeProgress()
-	val planDefaultProgress: Flow<Float> = planRepository.planDefault.computeProgress()
+    val planAllProgress: Flow<Float> = planRepository.planAll.computeProgress("All")
+    val planDefaultProgress: Flow<Float> = planRepository.planDefault.computeProgress("Default")
 
-	val plansProgress: Flow<PlanProgress> = channelFlow {
-		planRepository.standardPlans.collect { plans ->
-			for (plan in plans) {
-				launch {
-					var progress = progressRepository.getProgress(plan.id) ?: 0f
-					channel.send(PlanProgress(plan.id, progress))
+    val s: Flow<PlanProgress> = planRepository.standardPlans.transform {
 
-					progress = computePlanProgress(plan.id)
-					channel.send(PlanProgress(plan.id, progress))
-				}
-			}
-		}
-	}.flowOn(Dispatchers.Default)
+    }
+    val plansProgress: Flow<PlanProgress> = planRepository.standardPlans.run {
+        channelFlow {
+            logger.d(LOG_TAG, "standardPlan: start computing progress")
+            collect { plans ->
+                for (plan in plans) {
+                    launch {
+                        logger.d(LOG_TAG, "standardPlan: computing progress for $plan")
+                        var progress = progressRepository.getProgress(plan.id) ?: 0f
+                        send(PlanProgress(plan.id, progress))
 
-	fun update() {
-		_update.value = Any()
-	}
+                        progress = computePlanProgress(plan.id)
+                        send(PlanProgress(plan.id, progress))
+                    }
+                }
+            }
+        }.flowOn(Dispatchers.Default)
+    }
 
-	private fun Flow<Plan>.computeProgress(): Flow<Float> = combine(_update) { plan, _ -> plan }
-			.transform { plan ->
-				var progress = progressRepository.getProgress(plan.id) ?: 0f
-				emit(progress)
+    fun update() {
+        _update.value = Any()
+        logger.d(LOG_TAG, "update()")
+    }
 
-				progress = computePlanProgress(plan.id)
-				emit(progress)
-			}.flowOn(Dispatchers.Default)
+    private fun Flow<Plan>.computeProgress(planName: String): Flow<Float> =
+        combine(_update) { plan, _ -> plan }
+            .transform { plan ->
+                logger.d(logTag, "$planName: computing progress")
+                var progress = progressRepository.getProgress(plan.id) ?: 0f
+                emit(progress)
+
+                progress = computePlanProgress(plan.id)
+                emit(progress)
+            }.flowOn(Dispatchers.Default)
+
+    companion object {
+
+        private const val LOG_TAG = "GetProgressFromAllPlan"
+    }
 }
