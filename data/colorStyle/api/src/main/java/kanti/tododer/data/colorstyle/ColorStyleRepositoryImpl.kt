3f54e40500bb644a53b0a3407d12f7e6574ea7ce
Kirill Zhitko <kirillzhitko@gmail.com>
2024-02-20 15:44:03 +0300

    FEAT: вычисления в слое данных ColorStyle происходят в Dispatchers.Default

--- data/colorStyle/api/src/main/java/kanti/tododer/data/colorstyle/ColorStyleRepositoryImpl.kt
+++ data/colorStyle/api/src/main/java/kanti/tododer/data/colorstyle/ColorStyleRepositoryImpl.kt
@@ -3,6 +3,8 @@ package kanti.tododer.data.colorstyle
 import kanti.tododer.data.colorstyle.datasource.local.ColorStyleLocalDataSource
 import kanti.tododer.util.log.Logger
 import kanti.tododer.util.log.StandardLog
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class ColorStyleRepositoryImpl @Inject constructor(
@@ -11,40 +13,45 @@ class ColorStyleRepositoryImpl @Inject constructor(
 ) : ColorStyleRepository {
 
     override suspend fun getAllColorStyleData(): List<ColorStyleData> {
-        val colorStylesData = ArrayList<ColorStyleData>()
-        for (colorStyleId in DefaultColorStyles.Ids.values()) {
-            try {
-                val data = getOrInit(
-                    get = { localDataSource.getColorStyleData(colorStyleId) },
-                    init = {
-                        val colorStyle = DefaultColorStyles.getById(colorStyleId)
-                        localDataSource.insert(colorStyle)
-                    }
-                )
-                colorStylesData.add(data)
-            } catch (ex: Throwable) {
-                logger.e(LOG_TAG, ex.localizedMessage, ex)
+        val colorStylesData = withContext(Dispatchers.Default) {
+            val colorStylesData = ArrayList<ColorStyleData>()
+            for (colorStyleId in DefaultColorStyles.Ids.values()) {
+                try {
+                    val data = getOrInit(
+                        get = { localDataSource.getColorStyleData(colorStyleId) },
+                        init = {
+                            val colorStyle = DefaultColorStyles.getById(colorStyleId)
+                            localDataSource.insert(colorStyle)
+                        }
+                    )
+                    colorStylesData.add(data)
+                } catch (ex: Throwable) {
+                    logger.e(LOG_TAG, ex.localizedMessage, ex)
+                }
             }
+            val customStylesData = localDataSource.getCustomStylesData()
+            colorStylesData.addAll(customStylesData)
+            colorStylesData
         }
-        val customStylesData = localDataSource.getCustomStylesData()
-        colorStylesData.addAll(customStylesData)
         logger.d(LOG_TAG, "getAllColorStyleData(): return count(${colorStylesData.size})")
         return colorStylesData
     }
 
     override suspend fun getById(id: Int): ColorStyle? {
-        val result = when (id) {
-            in DefaultColorStyles.Ids.values() -> {
-                getOrInit(
-                    get = { localDataSource.getColorStyle(id) },
-                    init = {
-                        val colorStyle = DefaultColorStyles.getById(id)
-                        localDataSource.insert(colorStyle)
-                    }
-                )
-            }
+        val result = withContext(Dispatchers.Default) {
+            when (id) {
+                in DefaultColorStyles.Ids.values() -> {
+                    getOrInit(
+                        get = { localDataSource.getColorStyle(id) },
+                        init = {
+                            val colorStyle = DefaultColorStyles.getById(id)
+                            localDataSource.insert(colorStyle)
+                        }
+                    )
+                }
 
-            else -> localDataSource.getColorStyle(id)
+                else -> localDataSource.getColorStyle(id)
+            }
         }
         logger.d(LOG_TAG, "getById(Int = $id): return $result")
         return result
--- data/colorStyle/impl/room/src/main/java/kanti/tododer/data/colorstyle/datasource/local/ColorStyleRoomDataSource.kt
+++ data/colorStyle/impl/room/src/main/java/kanti/tododer/data/colorstyle/datasource/local/ColorStyleRoomDataSource.kt
@@ -4,6 +4,8 @@ import kanti.tododer.data.colorstyle.ColorStyle
 import kanti.tododer.data.colorstyle.ColorStyleData
 import kanti.tododer.data.colorstyle.ColorStyleType
 import kanti.tododer.data.room.colorstyle.ColorStyleDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class ColorStyleRoomDataSource @Inject constructor(
@@ -11,20 +13,28 @@ class ColorStyleRoomDataSource @Inject constructor(
 ) : ColorStyleLocalDataSource {
 
     override suspend fun getColorStyleData(id: Int): ColorStyleData? {
-        return dao.getColorStyleData(id)?.toColorStyleData()
+        return withContext(Dispatchers.Default) {
+            dao.getColorStyleData(id)?.toColorStyleData()
+        }
     }
 
     override suspend fun getColorStyle(id: Int): ColorStyle? {
-        return dao.getColorStyle(id)?.toColorStyle()
+        return withContext(Dispatchers.Default) {
+            dao.getColorStyle(id)?.toColorStyle()
+        }
     }
 
     override suspend fun getCustomStylesData(): List<ColorStyleData> {
-        return dao.getColorStylesDataByType(ColorStyleType.Custom.name).map {
-            it.toColorStyleData()
+        return withContext(Dispatchers.Default) {
+            dao.getColorStylesDataByType(ColorStyleType.Custom.name).map {
+                it.toColorStyleData()
+            }
         }
     }
 
     override suspend fun insert(colorStyle: ColorStyle) {
-        dao.insert(colorStyle.toColorStyleEntity())
+        withContext(Dispatchers.Default) {
+            dao.insert(colorStyle.toColorStyleEntity())
+        }
     }
 }
