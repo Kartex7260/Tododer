786d4041c32724c28ac2da05312a17f90b11801c
Kirill Zhitko <kirillzhitko@gmail.com>
2024-02-22 16:37:11 +0300

    FIX: вылет при удалении текущего плана

--- data/appData/api/src/main/java/kanti/todoer/data/appdata/AppDataRepositoryImpl.kt
+++ data/appData/api/src/main/java/kanti/todoer/data/appdata/AppDataRepositoryImpl.kt
@@ -1,8 +1,8 @@
 package kanti.todoer.data.appdata
 
 import kanti.tododer.common.Const
-import kanti.tododer.util.log.StandardLog
 import kanti.tododer.util.log.Logger
+import kanti.tododer.util.log.StandardLog
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.onEach
@@ -25,37 +25,29 @@ class AppDataRepositoryImpl @Inject constructor(
 			logger.d(logTag, "currentPlanId: Flow<Long> onEach { $it }")
 			if (it == null) {
 				logger.d(logTag, "currentPlanId is null, set default plan id and filter")
+				mutex.lock()
 				setCurrentPlan(Const.PlansIds.DEFAULT)
 			} else {
-				mutex.tryLock().apply {
-					logger.d(logTag, "currentPlanId is $it, lock ($this)")
-				}
 				logger.d(logTag, "_currentId set $it")
 				_currentId = it
-				logger.d(logTag, "currentPlanId is $it, unlock")
-				mutex.unlock()
+				if (mutex.isLocked)
+					mutex.unlock()
 			}
 		}
 		.filterNotNull()
 
 	override suspend fun setCurrentPlan(planId: Long) {
 		logger.d(logTag, "setCurrentPlan(Long = $planId)")
-		mutex.tryLock().apply {
-			logger.d(logTag, "setCurrentPlan(Long = $planId): lock ($this)")
-		}
 		localDataSource.setCurrentPlan(planId)
 	}
 
 	override suspend fun deleteCurrentPlan() {
 		logger.d(logTag, "deleteCurrentPlan()")
-		mutex.tryLock().apply {
-			logger.d(logTag, "deleteCurrentPlan(): lock ($this)")
-		}
 		localDataSource.setCurrentPlan(null)
 	}
 
 	override suspend fun deleteIfCurrent(planId: Long) {
-		logger.d(logTag, "deleteIfCurrent(Long = $planId): mutex is lock (${mutex.isLocked})")
+		logger.d(logTag, "deleteIfCurrent(Long = $planId)")
 		mutex.withLock {
 			when (_currentId) {
 				planId -> {
